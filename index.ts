2196dd193998d30c1da72ba74cb3a5b38a268c9f
Christian Dionisio <christiandionisio9432@gmail.com>
2021-04-24 23:50:12 -0500

    Estructura del server

---
+++ index.ts
@@ -0,0 +1,9 @@
+import Server from './classes/server';
+
+
+const server = new Server();
+
+//Levantar express
+server.start(() => {
+    console.log(`Servidor corriendo en puerto ${server.port}`);
+});
