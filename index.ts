a2ff4707c4b1e10fac1703a5869aec63c5ba2b4f
Christian Dionisio <christiandionisio9432@gmail.com>
2021-04-25 14:11:41 -0500

    Posts con paginacion

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Server from './classes/server';
 import userRoutes from './routes/usuario';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import postRoutes from './routes/post';
 
 
 const server = new Server();
@@ -12,6 +13,7 @@ server.app.use(bodyParser.json());
 
 // Rutas de mi app
 server.app.use('/user', userRoutes);
+server.app.use('/posts', postRoutes);
 
 // Conectar DB
 mongoose.connect('mongodb://localhost:27017/fotosgram', 
