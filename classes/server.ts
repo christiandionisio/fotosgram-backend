489c9ef5cfc3e75e2c5d5b8c378d90483ed22ec8
Christian Dionisio <christiandionisio9432@gmail.com>
2021-05-02 01:09:33 -0500

    Add configuration for deploy

--- classes/server.ts
+++ classes/server.ts
@@ -11,7 +11,7 @@ export default class Server {
     }
 
     start(callback: any) {
-        this.app.listen(this.port, callback);
+        this.app.listen(process.env.PORT || this.port, callback);
     }
 
 }
